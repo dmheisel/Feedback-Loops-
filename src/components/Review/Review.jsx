788a5d3c816596e86c6067c7c90479600366a65a
dmheisel <d.m.heisel@gmail.com>
2019-08-24 09:42:17 -0500

    review page basics completed - post route setup

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+import axios from 'axios'
 
 class Review extends Component {
 
   handleSubmit = () => {
-
+    axios
+      .post('/feedback', this.props.feedback)
+      .then(response => {
+        console.log(`successful POST route to server: ${response}`)
+      })
+      .catch(err => {
+        console.log(`error on POST route to server: ${err}`)
+      })
   }
   render() {
     return (
       <div>
         <h1> Review your feedback:</h1>
         <div>
+          <h4>{JSON.stringify(this.props.feedback)}</h4>
           <h3>Feelings: {this.props.feedback.feeling}</h3>
           <h3>Understanding: {this.props.feedback.understanding}</h3>
           <h3>Support: {this.props.feedback.support}</h3>
